b20fb168b0b59221f7cba908c651524d213bc34a
Magma008 <unknownmagma008@gmail.com>
2025-12-15 21:51:23 +0500

    15 December  2025 Monday 09:51PM

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div className="main-container">
+        bdfhbhj
+      </div>
+    </div>
+  )
+}
+
+export default App
